10ce8eec3dd5f589f237cc2230447a72269d3585
Michal Cichra <michal@o2h.cz>
2015-06-05 17:24:08 +0200

    added example es6 files using modules

---
+++ app/assets/javascripts/greeter.js
@@ -0,0 +1,8 @@
+
+export class Greeter {
+
+    greet(person) {
+        return `Hello ${person}!`;
+    }
+
+}
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import {Greeter} from 'greeter.js';
+
+const greeter = new Greeter();
+
+console.log(greeter.greet('Michal'));
