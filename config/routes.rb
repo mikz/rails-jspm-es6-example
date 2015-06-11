544256cf395a8a23d020069e94c896da23068a75
Michal Cichra <michal@o2h.cz>
2015-06-11 16:03:46 +0200

    generate welcome controller
    $ rails new jspm_example
    $ cd jspm_example
    $ rails generate controller welcome index --no-helper --no-assets
    
    and added root route welcome#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'welcome#index'
 end
